d18a82d7e7281de4c44c9ea279305e163949bdc9
Krithck Kishore T <krithckkishore.t@gmail.com>
2025-07-28 19:04:07 +0530

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Load the secret stream key
+STREAM_KEY=$STREAM_KEY
+
+# Start FFmpeg stream to YouTube (adjust input as needed)
+ffmpeg \
+ -re -stream_loop -1 -i "https://archive.org/download/kk_20250727/kk.mp4" \
+ -vcodec libx264 -preset veryfast -b:v 6000k -maxrate 6000k -bufsize 12000k \
+ -acodec aac -b:a 160k -ar 44100 -ac 2 \
+ -pix_fmt yuv420p -g 120 -r 60 \
+ -f flv "rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY"
